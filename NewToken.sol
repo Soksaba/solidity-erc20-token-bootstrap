2fd6b6338b44ba6eaa8673089108e7e0bb8087de
Soksaba <166646740+Soksaba@users.noreply.github.com>
2024-04-10 10:51:20 -0700

    Update NewToken.sol

--- NewToken.sol
+++ NewToken.sol
@@ -4,10 +4,10 @@ import "./ERC20Standard.sol";
 
 contract NewToken is ERC20Standard {
 	constructor() public {
-		totalSupply = 123;
-		name = "Best ever coin";
+		totalSupply = 1 000 000;
+		name = "Sok coin";
 		decimals = 4;
-		symbol = "BEC";
+		symbol = "SOK";
 		version = "1.0";
 		balances[msg.sender] = totalSupply;
 	}
